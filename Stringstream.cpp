717623766e3e892f6ea550cf541b916ab732d791
Majharul Islam Rafat <rafatislam.syl@gmail.com>
2017-02-09 22:28:48 +0600

    Add files via upload

---
+++ Stringstream.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    int n;
+    stringstream ss(s);
+    while(ss>>n)
+    {
+        cout<<n<<endl;
+    }
+}
