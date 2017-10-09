0b3fd72104beeb698a593bf4f856c157cd389675
Majharul Islam Rafat <rafatislam.syl@gmail.com>
2017-10-09 22:59:52 +0600

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,53 +1,57 @@
-struct data{
-    int v,w;
-    bool operator<(const data &p)const
-    {
-        return p.w<w; ///in ascending order
-    }
-};
-
-vecotr<data>con[];dis[];
-//whoTo[];color[]
-
-bool relax(int u,int v,int w)
+#include<bits/stdc++.h>
+using namespace std;
+vector<pair<int,int> >vv[105];
+int dist[105];
+void bfs(int src)
 {
-    if(dis[v]>dis[u]+w)
-    {
-        dis[v]=dis[u]+w;
-        //whoTo[v]=u;
-        return true;
-    }
-    return false;
-}
-
-
-void dij(int start,int end)
-{
-    priority_queue<data>pq;
-    clear dis[] with inf;
-    dis[start]=0;
-    //whoTo[start]=start;
-    pq.push(data(start,0));
-    data tmp;
+    int node,temp,cost,weight;
+    dist[src]=0;
+    priority_queue<pair<int,int> >pq;
+    pq.push({src,0});
     while(!pq.empty())
     {
-        tmp=pq.top();
+        temp=pq.top().first;
+        cost=pq.top().second*-1;
         pq.pop();
-        //if(color[tmp.v]==0)
-        //{
-        //color[tmp.v]=1;
-        int u=tmp.v;
-        if(u==end)return;
-
-        for(int i=0;i<SZ(con[u]);i++)
+        for(int i=0; i<vv[temp].size(); i++)
         {
-            tmp=con[u][i];
-            if(relax(u,tmp.v,tmp.w))
+            node=vv[temp][i].second;
+            weight=vv[temp][i].first;
+            if(dist[node]==-1)
             {
-                pq.push(data(tmp.v,dis[tmp.v]));
+                dist[node]=weight+dist[temp];
+                pq.push({node,-1*dist[node]});
+            }
+            else if(dist[node]>dist[temp]+weight)
+            {
+                dist[node]=dist[temp]+weight;
+                pq.push({node,-1*dist[node]});
             }
         }
-        //} END OF COLOR BLOCK
-     }
-     return;
+    }
 }
+int main()
+{
+    int n,m,t,from,u,v,w;
+    scanf("%d",&t);
+    for(int cs=1; cs<=t; cs++)
+    {
+        memset(dist,-1,sizeof(dist));
+        scanf("%d%d",&n,&m);
+        for(int i=1; i<=m; i++)
+        {
+            scanf("%d%d%d",&u,&v,&w);
+            vv[u].push_back({w,v});
+            vv[v].push_back({w,u});
+        }
+        bfs(1);
+        printf("Case %d: ",cs);
+        if(dist[n]==-1)
+            printf("Impossible\n");
+        else
+            printf("%d\n",dist[n]);
+        for(int i=0; i<105; i++)
+            vv[i].clear();
+    }
+}
+
