e310f33c8a3a715facf2a86592f1a122e59c0474
Majharul Islam Rafat <rafatislam.syl@gmail.com>
2019-10-17 12:55:45 +0600

    Update BPM with Hopcroft.cpp

--- BPM with Hopcroft.cpp
+++ BPM with Hopcroft.cpp
@@ -1,205 +1,64 @@
-// Tested in lightoj 1201 //
-
-#include <sstream>
-#include <queue>
-#include <set>
-#include <map>
-#include <cstdio>
-#include <cstdlib>
-#include <cctype>
-#include <cmath>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <bitset>
-#include <list>
-#include <stack>
-#include <string.h>
-#include <assert.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define print1(a)    cout<<a<<endl
-#define print2(a,b) cout<<a<<" "<<b<<endl
-#define print3(a,b,c) cout<<a<<" "<<b<<" "<<c<<endl
-#define oo          (1<<30)
-#define PI          3.141592653589793
-#define pi          (2*acos(0))
-#define ERR        1e-5
-#define PRE        1e-8
-#define SZ(s)      ((int)s.size())
-#define LL           long long
-#define ISS         istringstream
-#define OSS        ostringstream
-#define VS          vector<string>
-#define VI          vector<int>
-#define VD          vector<double>
-#define VLL         vector<long long>
-#define SII         set<int>::iterator
-#define SI          set<int>
-#define mem(a,b)    memset(a,b,sizeof(a))
-#define fr(i,a,b)   for(i=a;i<=b;i++)
-#define frn(i,a,b)  for(i=a;i>=b;i--)
-#define fri(a,b)    for(i=a;i<=b;i++)
-#define frin(a,b)   for(i=a;i>=b;i--)
-#define frj(a,b)    for(j=a;j<=b;j++)
-#define frjn(a,b)   for(j=a;j>=b;j--)
-#define frk(a,b)    for(k=a;k<=b;k++)
-#define frkn(a,b)   for(k=a;k>=b;k--)
-#define frl(a,b)    for(l=a;l<=b;l++)
-#define frln(a,b)   for(l=a;l>=b;l--)
-#define EQ(a,b)     (fabs(a-b)<ERR)
-#define all(a,b,c)  for(int I=0;I<b;I++)    a[I] = c
-#define CROSS(a,b,c,d) ((b.x-a.x)*(d.y-c.y)-(d.x-c.x)*(b.y-a.y))
-#define sqr(a)      ((a)*(a))
-#define FORE(i,a)   for(typeof((a).begin())i=(a).begin();i!=(a).end();i++)
-#define typing(j,b) typeof((b).begin()) j=(b).begin();
-#define BE(a)       a.begin(),a.end()
-#define rev(a)      reverse(BE(a));
-#define sorta(a)    sort(BE(a))
-#define pb          push_back
-#define popb        pop_back
-#define round(i,a)  i = ( a < 0 ) ? a - 0.5 : a + 0.5;
-#define makeint(n,s)  istringstream(s)>>n
-#define inpow(a,x,y) int i; a=x;fri(2,y)  a*=x
-#define cntbit(mask) __builtin_popcount(mask)
-#define lim          1005  //number of nodes(yes/no nodes)
-#define csprnt printf("Case %d: ", ++cas);
-template<class T1> void debug(T1 e)
-{
-    cout<<e<<endl;
-}
-template<class T1,class T2> void debug(T1 e1,T2 e2)
-{
-    cout<<e1<<"\t"<<e2<<endl;
-}
-template<class T1,class T2,class T3> void debug(T1 e1,T2 e2,T3 e3)
-{
-    cout<<e1<<"\t"<<e2<<"\t"<<e3<<endl;
-}
-template<class T1,class T2,class T3,class T4> void debug(T1 e1,T2 e2,T3 e3,T4 e4)
-{
-    cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<endl;
-}
-template<class T1,class T2,class T3,class T4,class T5> void debug(T1 e1,T2 e2,T3 e3,T4 e4,T5 e5)
-{
-    cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<"\t"<<e5<<endl;
-}
-template<class T1,class T2,class T3,class T4,class T5,class T6> void debug(T1 e1,T2 e2,T3 e3,T4 e4,T5 e5,T6 e6)
-{
-    cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<"\t"<<e5<<"\t"<<e6<<endl;
-}
-//*************************************************My Code Starts Here*********************************************************************************
-//Esqrt(V) Complexity
-//0 Based
-//Edge from set a to set b
-const int MAXN1 = 50010; //nodes in set a
-const int MAXN2 = 50010; //nodes in set b
-const int MAXM = 150010; //number of edges
+const int MX=500005;
+int num[MX],arr[MX];
+vector<int>prime;
+int n,m,k,x,vis[MX],start[MX],_left[MX],_right[MX],ee,a,b; // n means node
+struct edge{
+    int v,_next;
+    edge(){}
+    edge(int _v,int __next){v=_v,_next=__next;}
+}edgeList[500000];
 
-int n1, n2, edges, last[MAXN1], prevs[MAXM], head[MAXM];
-int matching[MAXN2], dist[MAXN1], Q[MAXN1];
-bool used[MAXN1], vis[MAXN1]; //vis is cleared in each dfs
+void init(){
+    ee=0;
+    memset(start,-1,sizeof(start));
+    return;
+}
+int cnt=1;
 
-// n1 = number of nodes in set a, n2 = number of nodes in set b
-void init(int _n1, int _n2)
-{
-    n1 = _n1;
-    n2 = _n2;
-    edges = 0;
-    fill(last, last + n1, -1);
+void addEdge(int u,int v){
+    u++;
+    v++;
+    edgeList[ee]=edge(v,start[u]);start[u]=ee;ee++;
+    return;
 }
 
-void addEdge(int u, int v)
-{
-    head[edges] = v;
-    prevs[edges] = last[u];
-    last[u] = edges++;
-}
-
-void bfs()
-{
-    fill(dist, dist + n1, -1);
-    int sizeQ = 0;
-    for (int u = 0; u < n1; ++u)
-    {
-        if (!used[u])
-        {
-            Q[sizeQ++] = u;
-            dist[u] = 0;
+bool dfs(int src){
+    if(vis[src]==x)return false;
+    vis[src]=x;
+    for(int i=start[src];i!=-1;i=edgeList[i]._next){
+        if(_right[edgeList[i].v]==-1){
+            _right[edgeList[i].v]=src;
+            _left[src]=edgeList[i].v;
+            return true;
         }
     }
-    for (int i = 0; i < sizeQ; i++)
-    {
-        int u1 = Q[i];
-        for (int e = last[u1]; e >= 0; e = prevs[e])
-        {
-            int u2 = matching[head[e]];
-            if (u2 >= 0 && dist[u2] < 0)
-            {
-                dist[u2] = dist[u1] + 1;
-                Q[sizeQ++] = u2;
-            }
-        }
-    }
-}
-
-bool dfs(int u1)
-{
-    vis[u1] = true;
-    for (int e = last[u1]; e >= 0; e = prevs[e])
-    {
-        int v = head[e];
-        int u2 = matching[v];
-        if (u2 < 0 || (!vis[u2] && dist[u2] == dist[u1] + 1 && dfs(u2)))
-        {
-            matching[v] = u1;
-            used[u1] = true;
+    for(int i=start[src];i!=-1;i=edgeList[i]._next){
+        if(dfs(_right[edgeList[i].v])){
+            _right[edgeList[i].v]=src;
+            _left[src]=edgeList[i].v;
             return true;
         }
     }
     return false;
 }
-
-int augmentPath()
-{
-    bfs();
-    fill(vis, vis + n1, false);
-    int f = 0;
-    for (int u = 0; u < n1; ++u)
-        if (!used[u] && dfs(u))
-            ++f;
-    return f;
-}
-
-int maxMatching()
-{
-    fill(used, used + n1, false);
-    fill(matching, matching + n2, -1);
-    for (int res = 0;;)
-    {
-        int f = augmentPath();
-        if (!f)
-            return res;
-        res += f;
-    }
-}
-int main()
-{
-    int n,cs=1,m,p,a,b,t;
-    scanf("%d",&t);
-    for(int cs=1;cs<=t;cs++)
-    {
-        scanf("%d%d",&n,&m);
-        init(n,n);
-        for(int i=1;i<=m;i++)
-        {
-            scanf("%d%d",&a,&b);
-            a--;
-            b--;
-            addEdge(a,b);
-            addEdge(b,a);
+int HopCroft(){
+    
+    init();
+    memset(_left,-1,sizeof(_left));
+    memset(_right,-1,sizeof(_right));
+    bool done=false;
+    while(!done){
+        x++;
+        done=true;
+        for(int i=1;i<=n;i++){
+            if(_left[i]==-1 && dfs(i)){
+                done=false;
+            }
         }
-        // BPM= maxMatching();
-        printf("Case %d: %d\n",cs,n-maxMatching()/2);
     }
+    int ret=0;
+    for(int i=1;i<=n;i++)ret+=(_left[i]!=-1);
+    return ret>>1;
 }
